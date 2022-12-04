1d68b9b494dbe772fcef3c6efabfd60ef6eae85a
tomoki <benzousan7@gmail.com>
2022-12-04 22:40:16 +0900

    編集機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,19 @@ class PostsController < ApplicationController
   def show
     @post=Post.find(params[:id])
   end
+
+  def edit
+    @post=Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   
   def post_params
