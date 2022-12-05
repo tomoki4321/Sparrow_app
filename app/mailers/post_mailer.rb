9e3e2ec8e99a7d0941a4e151c23abf9a5a3c7f82
tomoki <benzousan7@gmail.com>
2022-12-05 14:27:02 +0900

    送信機能を再々修正

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,6 +2,6 @@ class PostMailer < ApplicationMailer
   def post_mail(post)
     @post = post
 
-    mail to: @post.email, subject: "お問い合わせの確認メール"
+    mail to: @post.user.email, subject: "お問い合わせの確認メール"
   end
 end
