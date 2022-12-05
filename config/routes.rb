1ba9ca68cd2f6dcbaa26a4e5484fcbb74102a9c4
tomoki <benzousan7@gmail.com>
2022-12-05 11:35:28 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to 'users#new'
+  root to: 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new,:create,:edit,:show,:update]
   resources :favorites, only: [:create, :destroy,:index]
