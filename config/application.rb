3bb47e0ca8bc742745e7b4cb865ff628367e6fae
tomoki <benzousan7@gmail.com>
2022-12-05 11:11:12 +0900

    国際化を実装

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +11,8 @@ module SparrowApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
     #
